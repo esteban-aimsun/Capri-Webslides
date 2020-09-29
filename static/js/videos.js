7dbc95d6ac11bdeece7a848cae5cda176cdd3a71
juanep94 <52572269+juanep94@users.noreply.github.com>
2020-09-29 10:44:49 +0200

    New videos and automatic change

---
+++ static/js/videos.js
@@ -0,0 +1,75 @@
+/* Extra code for videos */
+
+
+var vid2 = document.getElementById("p5-40");
+var vid3 = document.getElementById("p5-60");
+var vid4 = document.getElementById("p5-80");
+var vid5 = document.getElementById("p5-100");
+var vid6 = document.getElementById("p15-20");
+var vid7 = document.getElementById("p15-40");
+var vid8 = document.getElementById("p15-60");
+var vid9 = document.getElementById("p15-80");
+var vid10 = document.getElementById("p15-100");
+var vid11 = document.getElementById("p25-20");
+var vid12 = document.getElementById("p25-40");
+var vid13 = document.getElementById("p25-60");
+var vid14 = document.getElementById("p25-80");
+var vid15 = document.getElementById("p25-100");
+
+vid2.onended = function() {
+        window.location.href="keynote-extra-slides.html#slide=2";
+    };
+
+vid3.onended = function() {
+        window.location.href="keynote-extra-slides.html#slide=4";
+    };
+
+vid4.onended = function() {
+        window.location.href="keynote-extra-slides.html#slide=6";
+    };
+
+vid5.onended = function() {
+        window.location.href="keynote-extra-slides.html#slide=8";
+    };
+
+vid6.onended = function() {
+        window.location.href="keynote-extra-slides.html#slide=11";
+    };
+
+vid7.onended = function() {
+        window.location.href="keynote-extra-slides.html#slide=13";
+    };
+
+vid8.onended = function() {
+        window.location.href="keynote-extra-slides.html#slide=15";
+    };
+
+vid9.onended = function() {
+        window.location.href="keynote-extra-slides.html#slide=17";
+    };
+
+vid10.onended = function() {
+        window.location.href="keynote-extra-slides.html#slide=19";
+    };
+
+vid11.onended = function() {
+        window.location.href="keynote-extra-slides.html#slide=22";
+    };
+
+vid12.onended = function() {
+        window.location.href="keynote-extra-slides.html#slide=24";
+    };
+
+vid13.onended = function() {
+        window.location.href="keynote-extra-slides.html#slide=26";
+    };
+
+vid14.onended = function() {
+        window.location.href="keynote-extra-slides.html#slide=28";
+    };
+
+vid15.onended = function() {
+        window.location.href="keynote-extra-slides.html#slide=30";
+    };
+
+
